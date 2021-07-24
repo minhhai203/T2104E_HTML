0a94f863f9d781790de9e6657383d5cce8938cdb
Đặng Minh Hải <637817@sv.vnua.edu.vn>
2021-07-24 19:33:41 +0700

    update thoi tiet

--- js/thoitiet.js
+++ js/thoitiet.js
@@ -21,7 +21,8 @@ $.ajax({
             }
         var row = $("img-weather");
             var icon = rs.weather[0].icon;
-            var ct = "<img class=\"icon1\" src=\"http://openweathermap.org/img/w/"+ icon +".png\">\n" +
+            console.log(icon);
+            var ct = "<img class=\"icon1\" src=\"https://openweathermap.org/img/w/"+ icon +".png\">";
             row.append(ct);      
     }
 });
@@ -30,7 +31,7 @@ function loadWeather() {
     var p = [];
     $.ajax({ 
         url:"https://api.openweathermap.org/data/2.5/forecast",
-        method:"GET",// GET hay POST do API quyet dinh
+        method:"GET",
         data:{
             q:"Hanoi,vietnam",
             units:"metric",
@@ -39,13 +40,13 @@ function loadWeather() {
         success:function (rs){ 
             p = rs.list;
             var row = $("#weather");
-            for (var i = 0; i < 12; i++) {
+            for (var i = 0; i < p.length; i++) {
                 var icon = p[i].weather[0].icon;
                 console.log(icon);
                 var ct = "<div class=\"item col-sm-3\">\n" +
                     "            <span class=\"time\">" + p[i].dt_txt + "</span>\n" +
                     "            <br>\n" +
-                    "            <img class=\"icon\" src=\"https://openweathermap.org/img/w/"+ icon +".png\">\n" +
+                    "            <img class=\"icon\" src=\"https://openweathermap.org/img/wn/"+ icon +"@2x.png\">\n" +
                     "            <h3 class=\"temp-max\">" + p[i].main.temp_max + " <sup>o</sup><sup>C</sup></h3>\n" +
                     "            <span class=\"temp-min\">" + p[i].main.temp_min + "</span>\n" +
                     "        </div>"; 
